1abd5c4ed7bf6ddf3f3814659914a8cc9a206c80
rakuyama <akiyoshi.yamauchi@rakus.co.jp>
2017-03-30 17:38:39 +0900

    fix メモの場合、自分自身にだけ投稿文を表示する。

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -2,9 +2,26 @@
 
 // メモを画面上に表示する
 function memo() {
-    const userName = '';
-    const message = '';
-    $('#thread').prepend('<p>' + '</p>');
-    
-    return false;
+  const userName = $('#userName').val();
+  const message = $('#message').val();
+
+  if (message.length < 1) {
+      alert('なにかにゅうりょくしてください！！！');
+      return;
+  }
+
+  const data = {
+     userName:userName
+    ,message:message
+  };
+
+  var msg = '';
+  msg += data.userName;
+  msg += 'さん: ';
+  msg += data.message;
+  $('#thread').prepend('<p>' + msg + '</p>');
+
+  $('#message').val('');
+
+  return false;
 }

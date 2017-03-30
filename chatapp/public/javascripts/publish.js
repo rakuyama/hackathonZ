3dd5491b27cfde509267b2d5cd879b12b74cfc88
rakuyama <akiyoshi.yamauchi@rakus.co.jp>
2017-03-30 17:25:56 +0900

    fix 投稿したら、自分を含めすべてのクライアントに投稿文を表示する。

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -2,13 +2,24 @@
 
 // 投稿メッセージをサーバに送信する
 function publish() {
-    const userName = '';
-    const message = '';
+    const userName = $('#userName').val();
+    const message = $('#message').val();
+
+    var dct = {
+       userName:userName
+      ,message:message
+    };
+
+    socket.emit('eve_pub',dct);
 
     return false;
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('eve_pub', function (data) {
+    var msg = '';
+    msg += data.userName;
+    msg += 'さん: ';
+    msg += data.message;
+    $('#thread').prepend('<p>' + msg + '</p>');
 });
--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -2,7 +2,21 @@
 
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
-    socket.on('', function (data) {
-        
+    socket.on('eve_pub', function (data) {
+      if (!data){
+        return;
+      }
+      if (!data.userName) {
+          return;
+      }
+      if (!data.message) {
+          return;
+      }
+
+      console.log('eve_pub: ' + data.userName);
+      console.log('eve_pub: ' + data.message);
+
+      io.sockets.emit('eve_pub', data);
+
     });
 };

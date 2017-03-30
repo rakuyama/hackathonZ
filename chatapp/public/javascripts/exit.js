5c5d40895efde9f8cb8474d4ee57e05a54e9ba72
rakuyama <akiyoshi.yamauchi@rakus.co.jp>
2017-03-30 19:15:32 +0900

    fix 退室メッセージ

--- chatapp/public/javascripts/exit.js
+++ chatapp/public/javascripts/exit.js
@@ -2,13 +2,21 @@
 
 // 退室メッセージをサーバに送信する
 function exit() {
-    const userName = '';
-    
+    const data = {
+        userName: $('#userName').val()
+    };
+    socket.emit('eve_exi',data);
+
     // 退室
     location.href = '/';
 }
 
+
 // サーバから受信した退室メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('eve_exi', function (data) {
+    var msg = '';
+    msg += '[' + ts2dt(data.timestamp) + '] '
+    msg += data.userName;
+    msg += 'さんが退室しました。';
+    $('#thread').prepend('<p>' + msg + '</p>');
 });
--- chatapp/sockets/exit.js
+++ chatapp/sockets/exit.js
@@ -2,7 +2,18 @@
 
 module.exports = function (socket) {
     // 退室メッセージをクライアントに送信する
-    socket.on('', function (data) {
-        
+    socket.on('eve_exi', function (data) {
+      if (!data){
+        return;
+      }
+      if (!data.userName) {
+          return;
+      }
+      data.timestamp = parseInt((new Date) / 1000);
+
+      console.log('eve_exi: ' + data.userName);
+      console.log('eve_exi: ' + data.timestamp);
+
+      socket.broadcast.emit('eve_exi', data);
     });
 };
